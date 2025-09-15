5e7842d17ecd40d2a1bab57a81b389f6f5f2a2b4
SangJae Lee <dev.codemaestro@gmail.com>
2025-09-16 00:13:35 +0900

    리액트 입문 설치내용 보완

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import MyComponent from "./components/MyComponents";
+
+function App() {
+  return <MyComponent />;
+}
+
+export default App;
---
+++ my-react-app/src/components/MyComponents.jsx
@@ -0,0 +1,10 @@
+// 리액트 컴포넌트의 기본적인 모습
+function MyComponent() {
+  return (
+    <div>
+      <p>Hello, React!</p>
+    </div>
+  );
+}
+
+export default MyComponent;
